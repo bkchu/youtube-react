2eae1812e193a5bac218441316d88cbea9694cd6
bkchu <brandon.k.chung@gmail.com>
2018-02-07 00:46:39 -0600

    fixed API calls to youtube search pacakage, added copy to clipboard button

--- src/components/VideoDetail/VideoDetail.js
+++ src/components/VideoDetail/VideoDetail.js
@@ -1,37 +1,48 @@
-import React from "react";
+import React, { Component } from "react";
 import moment from "moment";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
-const videoDetail = props => {
-  if (!props.video || !props.searchable) {
-    return <div className="loading" />;
+class videoDetail extends Component {
+  render() {
+    if (!this.props.video || !this.props.searchable) {
+      return <div className="loading" />;
+    }
+    const videoId = this.props.video.id;
+    const url = `https://www.youtube.com/embed/${videoId}?autoplay=1&rel=0`;
+    const noEmbedUrl = `https://www.youtube.com/watch?v=${videoId}`;
+    const content = this.props.video;
+    let channelUrl = `https://www.youtube.com/channel/${content.channelId}`;
+
+    return (
+      <div className="VideoDetail">
+        <div className="video-container embed-responsive embed-responsive-16by9">
+          <iframe
+            id={videoId}
+            className="embed-responsive-item"
+            title={content.title}
+            src={url}
+            frameBorder="0"
+            allowFullScreen
+          />
+        </div>
+
+        <CopyToClipboard text={noEmbedUrl}>
+          <button className="button-copy">Click to copy video link.</button>
+        </CopyToClipboard>
+
+        <p className="video-title">{content.title}</p>
+        <div className="description">
+          <a target="_blank" href={channelUrl}>
+            {content.channelTitle}
+          </a>
+          <p>
+            Published on {moment(content.publishedAt).format("MMM DD, YYYY")}
+          </p>
+          <p className="video-description">{content.description}</p>
+        </div>
+      </div>
+    );
   }
-  const { videoId } = props.video.id;
-  const url = `https://www.youtube.com/embed/${videoId}?autoplay=1&rel=0`;
-  const content = props.video.snippet;
-  let channelUrl = `https://www.youtube.com/channel/${content.channelId}`;
-
-  return (
-    <div className="VideoDetail">
-      <div className="video-container embed-responsive embed-responsive-16by9">
-        <iframe
-          id={videoId}
-          className="embed-responsive-item"
-          title={content.title}
-          src={url}
-          frameBorder="0"
-          allowFullScreen
-        />
-      </div>
-      <p className="video-title">{content.title}</p>
-      <div className="description">
-        <a target="_blank" href={channelUrl}>
-          {content.channelTitle}
-        </a>
-        <p>Published on {moment(content.publishedAt).format("MMM DD, YYYY")}</p>
-        <p className="video-description">{content.description}</p>
-      </div>
-    </div>
-  );
-};
+}
 
 export default videoDetail;
